10c8f963740723d966f253bbdacf384c442c3f8f
PolyakovIlya <9213539@gmail.com>
2019-01-30 12:37:20 +0200

    Updated logger, write combine, error logs, view in console.

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,12 +1,13 @@
 import winston from 'winston';
 
 export default winston.createLogger({
+    level: (process.env.NODE_ENV === 'production') ? 'info' : 'debug',
+    timestamp: true,
+    stderrLevels: ['error'],
+    colorize: true,
     transports: [
-        new winston.transports.Console({
-            level: (process.env.NODE_ENV === 'production') ? 'info' : 'debug',
-            timestamp: true,
-            stderrLevels: ['error'],
-            colorize: true
-        })
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combine.log' })
     ]
 });
