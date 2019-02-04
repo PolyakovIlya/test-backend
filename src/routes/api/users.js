799f94bc9931473897220b27db19547334495f9f
PolyakovIlya <9213539@gmail.com>
2019-02-04 23:00:30 +0200

    Async-await for users queries.

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -6,80 +6,79 @@ import config from '../../config/index';
 
 const router = Router();
 
-router.get('/user/:id', (req, res, next) => {
-    const { id } = req.params;
+router.get('/user/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
 
-    models.User.findById(id).then((user) => {
+        const user = await models.User.findById(id);
         res.json(user);
-    }).catch((err) => {
+    } catch (err) {
         const error = new Error(`Can't find required user: ${err}`);
-        return next(error);
-    });
+        next(error);
+    }
 });
 
-router.get('/users', (req, res, next) => {
-    models.User.find().then((users) => {
+router.get('/users', async (req, res, next) => {
+    try {
+        const users = await models.User.find();
         res.json(users);
-    }).catch((err) => {
+    } catch (err) {
         const error = new Error(`Can't get all users: ${err}`);
-        return next(error);
-    });
+        next(error);
+    }
 });
 
-router.post('/users/login', (req, res, next) => {
-    const { username, password } = req.body.user;
+router.post('/users/login', async (req, res, next) => {
+    try {
+        const { username, password } = req.body.user;
+        const user = await models.User.findOne({ username });
 
-    models.User.findOne({
-        username: username
-    }).then((user) => {
-        // compare password
         const hash = crypto.pbkdf2Sync(password, user.salt, 10000, 32, 'sha512').toString('hex');
 
         if (user.password === hash) {
-            jwt.sign({ username, isAdmin: user.isAdmin }, config.secret, {
-                expiresIn: 86400 // expires in 24 hours
-            }, (err, token) => res.json({
+            const token = await jwt.sign({ username, isAdmin: user.isAdmin }, config.secret, { expiresIn: 86400 }); // 24 hours
+
+            res.json({
                 username,
                 isAdmin: user.isAdmin,
                 email: user.email,
                 token
-            }));
+            })
+        } else {
+            const error = new Error('Incorrect username or password');
+            next(error);
         }
-
-        const error = new Error('Incorrect username or password');
-        return next(error);
-    }).catch(() => {
+    } catch (err) {
         const error = new Error('Authentication failed! Please check the request');
-        return next(error);
-    });
+        next(error);
+    }
 });
 
-router.post('/users/register', (req, res, next) => {
-    const { username, email, isAdmin } = req.body.user;
+router.post('/users/register', async (req, res, next) => {
+    try {
+        const { username, email, isAdmin } = req.body.user;
 
-    const salt = crypto.randomBytes(16).toString('hex');
-    const password = crypto.pbkdf2Sync(req.body.user.password, salt, 10000, 32, 'sha512').toString('hex');
+        const salt = crypto.randomBytes(16).toString('hex');
+        const password = crypto.pbkdf2Sync(req.body.user.password, salt, 10000, 32, 'sha512').toString('hex');
 
-    const user = new models.User({
-        username,
-        email,
-        salt,
-        password,
-        isAdmin
-    });
-
-    user.save()
-        .then((user) => {
-            if (user) {
-                res.json(user);
-            } else {
-                const error = new Error('Can\'t create user');
-                return next(error);
-            }
-        }).catch(() => {
-            const error = new Error('Can\'t find or create user');
-            return next(error);
+        const user = await models.User.create({
+            username,
+            email,
+            salt,
+            password,
+            isAdmin
         });
+
+        if (user) {
+            res.json(user);
+        } else {
+            const error = new Error('Can\'t create user');
+            next(error);
+        }
+    } catch(e) {
+        const error = new Error('Can\'t find or create user');
+        next(error);
+    }
 });
 
 export default router;
