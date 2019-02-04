3f79ba8e66892465b4d74ec14fd2b68339b6fa8c
PolyakovIlya <9213539@gmail.com>
2019-02-05 00:05:50 +0200

    Async-await for suggestion queries, finished feature.

--- src/routes/api/suggestions.js
+++ src/routes/api/suggestions.js
@@ -4,97 +4,92 @@ import permission from '../../middlewares/permission'
 
 const router = Router();
 
-router.get('/', permission('admin'), (req, res, next) => {
-    models.Suggestion.find()
-        .sort({createdAt: 'desc'})
-        .exec()
-        .then((suggestions) => {
-            res.json(suggestions);
-        }).catch((err) => {
-            const error = new Error(`Can't get all suggestions: ${err}`);
-            return next(error);
-        });
+router.get('/', permission('admin'), async (req, res, next) => {
+    try {
+        const suggestions = await models.Suggestion.find()
+            .sort({createdAt: 'desc'})
+            .exec();
+
+        res.json(suggestions);
+    } catch (err) {
+        const error = new Error(`Can't get all suggestions: ${err}`);
+        next(error);
+    }
 });
 
-router.get('/:articleId', permission('admin'), (req, res, next) => {
-    const { articleId } = req.params;
+router.get('/:articleId', permission('admin'), async (req, res, next) => {
+    try {
+        const { articleId } = req.params;
+        const suggestions = await models.Suggestion.find({articleId});
 
-    models.Suggestion.find({
-        articleId
-    }).then((suggestions) => {
-            res.json(suggestions);
-        }).catch((err) => {
-            const error = new Error(`Can't find suggestions by articleId: ${err}`);
-            return next(error);
-        });
+        res.json(suggestions);
+    } catch (err) {
+        const error = new Error(`Can't find suggestions by articleId: ${err}`);
+        next(error);
+    }
 });
 
-router.post('/', (req, res, next) => {
-    const { text, articleId, paragraphId, status } = req.body.suggestion;
+router.post('/', async (req, res, next) => {
+    try {
+        const { text, articleId, paragraphId, status } = req.body.suggestion;
 
-    models.Suggestion.create({
-        text,
-        paragraph_id: paragraphId,
-        status,
-        articleId
-    }).then((suggestion) => {
-        models.Article.findById(articleId).then(article => {
-            if(article && status === 'approved') {
-                article.set(`paragraphs.${suggestion.paragraph_id}.paragraph`, suggestion.text);
-                article.save().then(() => {
-                    return res.status(200).json({
-                        suggestion,
-                        message: 'Successfully created suggestion and article'
-                    });
-                })
-            } else {
-                return res.status(200).json({
+        const suggestion = await models.Suggestion.create({
+            text,
+            paragraph_id: paragraphId,
+            status,
+            articleId
+        });
+
+        const article = await models.Article.findById(articleId);
+
+        if(article && status === 'approved') {
+            article.set(`paragraphs.${suggestion.paragraph_id}.paragraph`, suggestion.text);
+            article.save().then(() => {
+                res.status(200).json({
                     suggestion,
-                    message: 'Successfully created suggestion'
+                    message: 'Successfully created suggestion and article'
                 });
-            }
-        });
+            })
+        } else {
+            res.status(200).json({
+                suggestion,
+                message: 'Successfully created suggestion'
+            });
+        }
 
-    }).catch((err) => {
+    } catch (err) {
         const error = new Error(`Can't create suggestion: ${err}`);
-        return next(error);
-    });
+        next(error);
+    }
 });
 
-router.put('/:id', permission('admin'), (req, res, next) => {
-    const { id } = req.params;
-    const { status } = req.body;
+router.put('/:id', permission('admin'), async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { status } = req.body;
 
-    models.Suggestion.findOneAndUpdate(
-        {
-            _id: id
-        },
-        {
-            status
-        },
-        { new: true }
-        ).then((suggestion) => {
-            models.Article.findById(suggestion.articleId).then(article => {
-                if(article && status === 'approved') {
-                    article.set(`paragraphs.${suggestion.paragraph_id}.paragraph`, suggestion.text);
-                    article.save().then(() => {
-                        return res.status(200).json({
-                            data: suggestion,
-                            message: 'Successfully updated suggestion and article'
-                        });
-                    })
-                } else {
-                    return res.status(200).json({
-                        data: suggestion,
-                        message: 'Successfully updated suggestion'
-                    });
-                }
+        const suggestion = await models.Suggestion.findOneAndUpdate({ _id: id }, { status }, { new: true });
+
+        const article = await models.Article.findById(suggestion.articleId);
+
+        if(article && status === 'approved') {
+            article.set(`paragraphs.${suggestion.paragraph_id}.paragraph`, suggestion.text);
+            article.save().then(() => {
+                res.status(200).json({
+                    data: suggestion,
+                    message: 'Successfully updated suggestion and article'
+                });
+            })
+        } else {
+            res.status(200).json({
+                data: suggestion,
+                message: 'Successfully updated suggestion'
             });
-        })
-        .catch((err) => {
-            const error = new Error(`Can't update suggestion: ${err}`);
-            return next(error);
-        });
+        }
+    } catch (err) {
+        const error = new Error(`Can't update suggestion: ${err}`);
+        next(error);
+    }
 });
 
 router.delete('/:id', permission('admin'), (req, res, next) => {
@@ -107,7 +102,7 @@ router.delete('/:id', permission('admin'), (req, res, next) => {
         });
     }).catch((err) => {
         const error = new Error(`Can't delete suggestion: ${err}`);
-        return next(error);
+        next(error);
     });
 
 });
